fc51feee0dbe95c409dc778edfa1fc3ddd53973a
Pau <pau.sole@gracia.lasalle.cat>
2024-10-21 08:53:28 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] notes;
+
+        float[] scores = new float[5];
+
+        String[] noms = {"Pau", "Alex", "Maria", "Joel", "Carla"};
+        System.out.println(noms[1]);
+
+        for (int i = 0; i < noms.length; i++) {
+            System.out.println(noms[i]);
+        }
+    }
+}
